5984ee107c79abfe25b31989a2ffd1a3513d37f3
YeKc1M <15008181891@163.com>
2019-12-12 16:16:56 +0800

    add class NFANode, some basic function of NFA and NFANode

--- src/NFA.java
+++ src/NFA.java
@@ -1,2 +1,42 @@
+import java.util.HashMap;
+
 public class NFA {
+    private NFANode startState;
+    public NFA(){
+        startState=new NFANode();
+    }
+    public static void main(String[] args) {
+        //System.out.println("hello world!");
+
+        NFANode n=new NFANode();
+        n.addNextState('a',new NFANode());
+        System.out.println(n.getNextStates());
+        System.out.println(n.getNextStates().get('a')+" "+n.getNextStates().get('b'));
+    }
+}
+
+class NFANode{
+    private String tokenName=null;
+    private HashMap<Character,NFANode> nextStates;
+    public NFANode(){
+        nextStates=new HashMap<>();
+    }
+    public String getTokenType() throws Exception {
+        if(tokenName==null){
+            throw new Exception("cannot recognize this symbol");
+        }
+        return tokenName;
+    }
+
+    public void setTokenName(String tokenName) {
+        this.tokenName = tokenName;
+    }
+    public String getTokenName(){return tokenName;}
+    public void addNextState(Character c,NFANode nfaNode){
+        nextStates.put(c,nfaNode);
+    }
+    public HashMap getNextStates(){return nextStates;}
+    public String toString(){
+        return nextStates.toString();
+    }
 }
