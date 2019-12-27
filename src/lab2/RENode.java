c93b3003229186d9adb32a3d96f6b34954680243
YeKc1M <15008181891@163.com>
2019-12-27 11:10:02 +0800

    modify equals() and add a related test function

--- src/lab2/RENode.java
+++ src/lab2/RENode.java
@@ -6,14 +6,14 @@ import java.util.Set;
 public class RENode {
     private int waitingIndex;//the waiting element of the re
     private int no;//corresponding RE-index in Table
-    Set endSymbol;
+    Set endSymbol;//Set<String>
     public RENode(int no){
         endSymbol=new HashSet();
         this.no=no;
         waitingIndex=0;
     }
     public boolean equals(RENode reNode){
-        return no==reNode.no&&waitingIndex== reNode.waitingIndex;
+        return no==reNode.no&&waitingIndex== reNode.waitingIndex&&endSymbol.equals(reNode.endSymbol);
     }
     public void setWaitingIndex(int waitingIndex){
         this.waitingIndex=waitingIndex;
@@ -26,5 +26,15 @@ public class RENode {
     }
     public int getWaitingIndex(){
         return waitingIndex;
+    }
+
+    public static void main(String[] args){
+        //testSetEquals();
+    }
+    private static void testSetEquals(){
+        Set set1=new HashSet(),set2=new HashSet();
+        String s1=new String("abc"),s2=new String("abc"),s3=new String("ab");
+        set1.add(s1);set1.add(s3);set2.add(s2);set2.add(s3);
+        System.out.print(set1.equals(set2));
     }
 }
